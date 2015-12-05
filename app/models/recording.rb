703ee366305145099a3939ef944dfb23e47729d8
Craig Read <craigabread@gmail.com>
2015-12-06 10:30:21 +1100

    implementing audio and image uploads

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -7,15 +7,13 @@ class Recording < ActiveRecord::Base
   belongs_to :language
   belongs_to :phrase
 
-  has_attached_file \
-    :audio,
-    url: '/recordings/:class/:id/:attachment/:style.:extension',
-    path: ':rails_root/public/assets/:class/:id/:attachment/:style.:extension'
+  has_attached_file :audio
 
   validates :audio, attachment_content_type: {
     content_type: [
       'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3',
-      'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio'
+      'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio',
+      'audio/x-m4a'
     ]
   }
 end
