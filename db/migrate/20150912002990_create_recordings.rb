a77eedf64028764a6159a617128bcbb78c52499e
Craig Read <craigabread@gmail.com>
2015-12-06 09:40:33 +1100

    removing the unneccesary audio_url

--- db/migrate/20150912002990_create_recordings.rb
+++ db/migrate/20150912002990_create_recordings.rb
@@ -1,7 +1,6 @@
 class CreateRecordings < ActiveRecord::Migration
   def change
     create_table :recordings do |t|
-      t.string :audio_url
       t.datetime :recorded
       t.references :interview, index: true, foreign_key: true
       t.references :language, index: true, foreign_key: true
