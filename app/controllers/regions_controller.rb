9609cb724b3691573c82fa3bc1fe3692aaebb18f
Warwick Stone <uozu.aho@gmail.com>
2016-09-17 19:54:52 +1000

    extract municipalities/_table, use for index and regions.show

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -10,6 +10,7 @@ class RegionsController < ApplicationController
   # GET /regions/1
   # GET /regions/1.json
   def show
+    @municipalities = @region.municipalities
   end
 
   # GET /regions/new
