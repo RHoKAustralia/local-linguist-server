9f24f23b65c1387d52388e901d5b0f7689d71a59
Craig Read <craigabread@gmail.com>
2017-03-11 15:20:24 +1100

    WIP: fixed the study json output (#16)

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -18,12 +18,12 @@ class Phrase < ActiveRecord::Base
   has_attached_file :audio, url: URL, path: UPLOAD_PATH
 
   validates :audio, attachment_content_type: {
-    content_type: [
-      'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3',
-      'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio',
-      'audio/x-m4a'
-    ]
-  }
+              content_type: [
+                'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3',
+                'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio',
+                'audio/x-m4a'
+              ]
+            }
 
   def to_s
     english_text
--- app/views/phrases/index.json.jbuilder
+++ app/views/phrases/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@phrases) do |phrase|
-  json.extract! phrase, :id, :english_text, :audio, :image, :response_type_id
+  json.extract! phrase, :id, :english_text, :audio, :image, :response_type_id, :choices
   json.url phrase_url(phrase, format: :json)
 end
