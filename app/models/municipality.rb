62dc1ef6043210ab90d704d4eacdc1df770471a6
Craig Read <craigabread@gmail.com>
2017-03-29 19:15:00 +1100

    Updating spec for municipalities controller
    * Adding validation to the municipalities model

--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -5,6 +5,9 @@ class Municipality < ActiveRecord::Base
   belongs_to :region
   has_many :locales
 
+  validates :name, presence: true
+  validates :region_id, presence: true
+
   def full_name
     "#{region.name} - #{name}"
   end
