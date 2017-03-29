51051daf882e1ed4a3fe8152bb9c9ddbb5690715
Craig Read <craigabread@gmail.com>
2017-03-29 19:06:26 +1100

    Updating language controller spec
    * Adding validation for language name

--- spec/factories/languages.rb
+++ spec/factories/languages.rb
@@ -2,8 +2,12 @@ FactoryGirl.define do
   sequence :language_name do |n|
     "Language #{n}"
   end
+  sequence :language_code do |n|
+    "#{n}"
+  end
 
   factory :language do
     name { generate(:language_name) }
+    language_code { generate(:language_code) }
   end
 end
