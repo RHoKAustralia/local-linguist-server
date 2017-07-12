ba29f3558372a12ec0dbb46e10154cad5bfec704
Craig Read <craig.read@redbubble.com>
2017-07-12 18:23:46 +1000

    fixing locale index spec

--- spec/views/locales/index.html.erb_spec.rb
+++ spec/views/locales/index.html.erb_spec.rb
@@ -1,22 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "locales/index", type: :view do
+RSpec.describe 'locales/index', type: :view do
+  let(:municipality) { FactoryGirl.create(:municipality) }
   before(:each) do
     assign(:locales, [
       Locale.create!(
-        :name => "Name",
-        :municipality => nil
+        name: 'Name',
+        municipality: municipality
       ),
       Locale.create!(
-        :name => "Name",
-        :municipality => nil
+        name: 'Name',
+        municipality: municipality
       )
     ])
   end
 
-  it "renders a list of locales" do
+  it 'renders a list of locales' do
     render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select 'tr>td', text: 'Name'.to_s, count: 2
+    assert_select 'tr>td', text: municipality.name, count: 2
   end
 end
