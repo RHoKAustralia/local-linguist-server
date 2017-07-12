3c6b5c0611b10ad1fb7e3256f4373bf29178d1f4
Craig Read <craig.read@redbubble.com>
2017-07-12 18:00:55 +1000

    fixing edit study spec

--- spec/views/studies/edit.html.erb_spec.rb
+++ spec/views/studies/edit.html.erb_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe "studies/edit", type: :view do
+RSpec.describe 'studies/edit', type: :view do
   before(:each) do
     @study = assign(:study, Study.create!(
-      :name => "MyString",
-      :language => nil
+      name: 'My Study',
+      start_date: '2017-07-01'
     ))
   end
 
-  it "renders the edit study form" do
+  it 'renders the edit study form' do
     render
 
-    assert_select "form[action=?][method=?]", study_path(@study), "post" do
+    assert_select 'form[action=?][method=?]', study_path(@study), 'post' do
 
-      assert_select "input#study_name[name=?]", "study[name]"
+      assert_select 'input#study_name[name=?]', 'study[name]'
 
-      assert_select "select#study_language_id[name=?]", "study[language_id]"
+      assert_select 'select#study_start_date_1i[name=?]', 'study[start_date(1i)]'
+      assert_select 'select#study_start_date_2i[name=?]', 'study[start_date(2i)]'
+      assert_select 'select#study_start_date_3i[name=?]', 'study[start_date(3i)]'
     end
   end
 end
