ca91b12a4c74417da0ebabd11f883200eb4193ca
Craig Read <craig.read@redbubble.com>
2017-07-14 08:32:15 +1000

    fixing phrases factory

--- spec/factories/phrases.rb
+++ spec/factories/phrases.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
 
   factory :phrase do |p|
     p.english_text { generate(:phrase_text) }
-    p.prompt { generate(:prompt_text) }
+    p.prompt_text { generate(:prompt_text) }
     p.study { |a| a.association(:study) }
     p.response_type_id 1
   end
