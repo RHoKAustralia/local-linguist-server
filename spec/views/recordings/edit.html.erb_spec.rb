988971bec68b77ed9aa507d3e168350ba03acaa4
Craig Read <craig.read@redbubble.com>
2017-07-14 08:32:26 +1000

    fixing edit recordings spec

--- spec/views/recordings/edit.html.erb_spec.rb
+++ spec/views/recordings/edit.html.erb_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "recordings/edit", type: :view do
+RSpec.describe 'recordings/edit', type: :view do
   before(:each) do
     @recording = assign(:recording, Recording.create!(
-      :audio => "",
-      :interview => nil,
-      :language => nil,
-      :phrase => nil
+      :audio => '',
+      :interview => FactoryGirl.create(:interview),
+      :phrase => FactoryGirl.create(:phrase)
     ))
   end
 
-  it "renders the edit recording form" do
+  it 'renders the edit recording form' do
     render
 
-    assert_select "form[action=?][method=?]", recording_path(@recording), "post" do
+    assert_select 'form[action=?][method=?]', recording_path(@recording), 'post' do
 
-      assert_select "input#recording_audio[name=?]", "recording[audio]"
+      assert_select 'input#recording_audio[name=?]', 'recording[audio]'
 
-      assert_select "select#recording_interview_id[name=?]", "recording[interview_id]"
+      assert_select 'select#recording_interview_id[name=?]', 'recording[interview_id]'
 
-      assert_select "select#recording_phrase_id[name=?]", "recording[phrase_id]"
+      assert_select 'select#recording_phrase_id[name=?]', 'recording[phrase_id]'
     end
   end
 end
