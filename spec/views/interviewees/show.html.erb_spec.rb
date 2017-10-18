e0efffe446d3237547faef2417ffa5b4e4e41a33
Craig Read <craig.read@redbubble.com>
2017-10-18 18:18:57 +1100

    fixed show interviewee view spec

--- spec/views/interviewees/show.html.erb_spec.rb
+++ spec/views/interviewees/show.html.erb_spec.rb
@@ -2,14 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "interviewees/show", type: :view do
   before(:each) do
-    @interviewee = assign(:interviewee, Interviewee.create!(
-      :name => "Name",
-      :mobile => "Mobile",
-      :email => "Email",
-      :gender => "Gender",
-      :occupation => "Occupation",
-      :education_level => "Education Level"
-    ))
+    @interviewee = assign(:interviewee, FactoryGirl.create(:interviewee))
   end
 
   it "renders attributes in <p>" do
@@ -19,6 +12,10 @@ RSpec.describe "interviewees/show", type: :view do
     expect(rendered).to match(/Email/)
     expect(rendered).to match(/Gender/)
     expect(rendered).to match(/Occupation/)
-    expect(rendered).to match(/Education Level/)
+    expect(rendered).to match(/Education level/)
+    expect(rendered).to match(/Age/)
+    expect(rendered).to match(/Locale/)
+    expect(rendered).to match(/Always lived here?/)
+    expect(rendered).to match(/First Language/)
   end
 end
